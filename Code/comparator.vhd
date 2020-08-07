58e8edaeb63cceb2c493825c0dba570f6c560f05
Konstantinos <ksakkasuoi@gmail.com>
2020-08-07 11:38:44 +0300

    comparator

--- Code/comparator.vhd
+++ Code/comparator.vhd
@@ -17,3 +17,19 @@ a_uns<=UNSIGNED(a);
 b_uns<=UNSIGNED(b);
 agtb<='1' WHEN a>b ELSE '0';
 END compare;
+
+--Το ίδιο με χρήση πακέτου ieee.std_logic_unsigned.all
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.std_logic_unsigned.all;
+------------------------------
+ENTITY comparator1 IS
+PORT(a, b : IN std_logic_vector(3 downto 0);
+agtb : OUT std_logic);
+END comparator1;
+------------------------------
+ARCHITECTURE compare OF comparator1 IS
+BEGIN
+agtb<='1' WHEN a>b ELSE '0';
+END compare;
+------------------------------
